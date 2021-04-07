0d4efc1f29554649e531d2694f633360198e28e2
Steve Larsen <slarsen-3@att.net>
2021-04-07 18:06:58 -0500

    removed unnecessary console.log

--- js/script.js
+++ js/script.js
@@ -9,7 +9,6 @@ $(document).ready(() => {
 
 const apiurl = "http://www.omdbapi.com/?apikey=dfe6d885&";
 const getMovie = async (searchTitle) => {
-  console.log("async fetch");
   try {
     const response = await (await fetch(apiurl + "s=" + searchTitle)).json();
     let movies = response.Search;
@@ -27,6 +26,51 @@ const getMovie = async (searchTitle) => {
     });
     console.log(movies);
     $("#movies").html(selected);
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+// pass data to poster.html
+function moreInfo(id) {
+  sessionStorage.setItem("movieId", id);
+  window.location = "poster.html";
+  return false;
+}
+
+const getMovieById = async () => {
+  let movieId = sessionStorage.getItem("movieId");
+  try {
+    const response = await (await fetch(apiurl + "i=" + movieId)).json();
+    console.log(response);
+    console.log(typeof response);
+    let poster = response.Poster;
+    let year = response.Year;
+    let genre = response.Genre;
+    let cast = response.Actors;
+    let title = response.Title;
+    let plot = response.Plot;
+    console.log(poster);
+    let selected = `
+    <div class="row">
+      <div>
+        <h2>${title}</h2>
+        <img src="${poster}">
+      </div>
+    </div>
+    <div class="row">
+    </hr>
+      <p>Released: ${year}</p>
+      <p>Genre: ${genre}</P>
+      <p>Cast: ${cast}</p>
+      <div class="poster">
+      <hr>
+      <h3>About</h3>
+      ${plot}
+      </div>
+    </div>    
+    `;
+    $("#movie").html(selected);
   } catch (err) {
     console.log(err);
   }
