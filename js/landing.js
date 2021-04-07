0cc0443049cd5f2738371b216eadcae917c9ea64
Steve Larsen <slarsen-3@att.net>
2021-04-07 18:04:44 -0500

    added ability to save to local storage

--- js/landing.js
+++ js/landing.js
@@ -2,8 +2,49 @@ const signInButton = document.getElementById("signInButton");
 const container = document.querySelector(".forms-container");
 signInButton.addEventListener("click", signInForm);
 
+const signUpButton = document.getElementById("signUpButton");
+const signUp = document.querySelector(".form-group");
+signUpButton.addEventListener("click", signUpForm);
+
 function signInForm() {
   signInButton.style.display = "none";
   container.style.display = "block";
 }
 
+function signUpForm() {
+  signUpButton.style.display = "none";
+  container.style.display = "none";
+  signUp.style.display = "block";
+}
+
+// if (window.localStorage) {
+//   console.log("supported");
+// } else {
+//   console.log("not supported");
+// }
+const memberSignUp = (event) => {
+  let formData = {
+    firstName: document.getElementById("firstName").value,
+    lastName: document.getElementById("lastName").value,
+    email: document.getElementById("userEmail").value,
+    password: document.getElementById("userPassword").value,
+  };
+  localStorage.setItem("formData", JSON.stringify(formData));
+  console.log(localStorage.getItem("formData"));
+  event.preventDefault();
+};
+
+function clearForm() {
+  document.getElementById("firstName").value = "";
+  document.getElementById("lastName").value = "";
+  document.getElementById("userEmail").value = "";
+  document.getElementById("userPassword").value = "";
+}
+
+function memberSignIn() {
+  if (userEmail.value == memberEmail && userPassword.value == memberPassword) {
+    alert("you're logged in");
+  } else {
+    alert("error");
+  }
+}
